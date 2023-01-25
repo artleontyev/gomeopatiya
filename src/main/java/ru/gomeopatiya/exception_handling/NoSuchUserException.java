81055f59c8d77ce25440bf133534476c3e3a3f41
artleontyev <42906948+artleontyev@users.noreply.github.com>
2023-01-25 21:59:39 +0300

    Delete src/main directory

--- src/main/java/ru/gomeopatiya/exception_handling/NoSuchUserException.java
+++
@@ -1,7 +0,0 @@
-package ru.gomeopatiya.exception_handling;
-
-public class NoSuchUserException extends RuntimeException {
-    public NoSuchUserException(String message) {
-        super(message);
-    }
-}
--- src/main/resources/static/for_admin/getUser.js
+++
@@ -1,7 +0,0 @@
-async function getUser(id) {
-    let requestUrlForGettingUser = 'http://localhost:8080/admin/api/users/' + id
-
-    let response = await fetch(requestUrlForGettingUser)
-        .then(res => res.json())
-    return response
-}
